085752b9679f6d1e993bcffb029489556683849c
Sean Tobin <stob17@gmail.com>
2018-09-23 14:59:52 -0400

    Update Process Summary Page.py
    Added function to scrape officers table when present

--- Process Summary Page.py
+++ Process Summary Page.py
@@ -5,18 +5,15 @@
 ###                     ###
 ###                     ###
 
-# By Sean Tobin, MPP. 
+# This is meant to replicate the public information as supplied by Michigan's Licensing and Regulatory Agency. 
 
-# This is meant to replicate the public information as supplied by the Michigan Department of Licensing and Regulatory Affairs. 
-#Load it into a database and have fun. I'm building a longitudinal spatial model. There is no warranty with this script. 
 
 import requests, os, time
 from bs4 import BeautifulSoup as bs
 
 import pandas as pd
 
-#Add your own Dir here
-#os.chdir('')
+## os.chdir('')
 
 def pageCompare(ID):
     # Goal here is to plug this function into a loop for the range described above to copy records of all businesses 
@@ -33,6 +30,7 @@ def pageCompare(ID):
 
 def page_parse(page):
     
+    # create empty dict with needed keys(columns)
     catch = dict.fromkeys(Column_IDs, [])
     
     for ID in Column_IDs:
@@ -43,67 +41,30 @@ def page_parse(page):
             pass
     return catch
 
-Column_IDs = [
-    'MainContent_lblEntityName',
-    'MainContent_lblIDNumberHeader',
-    'MainContent_lblEntityType',
-    'MainContent_lblOrganisationDate',
-    'MainContent_lblTerm',
-    'MainContent_lblMostRecentAnnualReportYear',
-    'MainContent_lblTerm',
-    'MainContent_lblInactiveDateLabel',
-    'MainContent_lblInactiveDate',
-    'MainContent_tdRevivalDateText',
-    'MainContent_tdInactiveDate',
-    'MainContent_lblMostRecentAnnualReportWithOfficersAndDirectors',
-    'MainContent_lblResidentAgentName',
-    'MainContent_lblResidentStreet',
-    'MainContent_lblaptsuiteother',
-    'MainContent_lblResidentCity',
-    'MainContent_lblResidentState',
-    'MainContent_lblResidentZip',
-    'MainContent_trConfPrinciple',
-    'MainContent_lblPrincipleStreet',
-    'MainContent_lblaptsuiteotherlblpricipal',
-    'MainContent_lblPrincipleCity',
-    'MainContent_lblPrincipleState',
-    'MainContent_lblPrincipleZip',
-    'MainContent_lblActsFormedUnder',
-    'MainContent_lblSum',
-    'MainContent_txtComments'
-]
-
-import requests, os, time
-from bs4 import BeautifulSoup as bs
-
-import pandas as pd
-
-os.chdir('C:\\Users\\Sean\\Desktop\\Forecasting')
-
-def pageCompare(ID):
-    # Goal here is to plug this function into a loop for the range described above to copy records of all businesses 
-    # available through this portal.
+def Officer_parse(page):
+    # builds a separate table for officers
     
-    baseLink = 'https://cofs.lara.state.mi.us/CorpWeb/CorpSearch/CorpSummary.aspx?ID='
+    # Grabs the html table - if it's there
+    tble = page.find(id = 'MainContent_grdOfficers')#'MainContent_tblOfficers') # MainContent_grdOfficers grid #
     
-    response = requests.get(baseLink + str(ID))
-    soup = bs(response.text, 'lxml')
-    message = soup.find(id = "MainContent_lblMessage")
+    rows = []
+    for x in tble.find_all('tr'):
+        cell =[cell.get_text() for cell in x.find_all('td')]
+        rows.append(cell)
     
-    if message is None:
-        return soup
+    # purge any empty rows
+    rows = [x for x in rows if len(x) > 0]
+    
+    # put records into DF
+    df = pd.DataFrame.from_records(rows, columns = ['Title', 'Name', 'Address'])
+    
+    # Add Corp ID to every row in this DF
+    ID = page.find(id='MainContent_lblIDNumberHeader').get_text()
+    df['CorpID'] = ID
+    
+    with open('Lara_officers_Ordered.csv', 'a') as f:
+            df.to_csv(f, header=False)    
 
-def page_parse(page):
-    
-    catch = dict.fromkeys(Column_IDs, [])
-    
-    for ID in Column_IDs:
-        catch[ID]= page.find(id=ID)
-        try:
-            catch[ID] = catch[ID].get_text()
-        except:
-            pass
-    return catch
 
 Column_IDs = [
     'MainContent_lblEntityName',
@@ -113,7 +74,6 @@ Column_IDs = [
     'MainContent_lblTerm',
     'MainContent_lblMostRecentAnnualReportYear',
     'MainContent_lblTerm',
-    'MainContent_lblInactiveDateLabel',
     'MainContent_lblInactiveDate',
     'MainContent_tdRevivalDateText',
     'MainContent_tdInactiveDate',
@@ -138,6 +98,8 @@ Column_IDs = [
 # Ok here is the meat and potatoes
 
 blank_df = pd.DataFrame([],columns = Column_IDs).to_csv('Lara_records_Ordered.csv')
+blank_df2 = pd.DataFrame([],columns = ['Title', 'Name', 'Address', 'CorpID']).to_csv('Lara_officers_Ordered.csv')
+
 
 for page in range(800000000,900000000):
     page_ID = pageCompare(page)
@@ -146,9 +108,12 @@ for page in range(800000000,900000000):
         
         foo = page_parse(page_ID)
         
+        if page_ID.find(id = 'MainContent_grdOfficers') is not None:
+            Officer_parse(page_ID)
+        
         df = pd.DataFrame(foo, index = [foo['MainContent_lblIDNumberHeader']],columns = Column_IDs)
         
         with open('Lara_records_Ordered.csv', 'a') as f:
             df.to_csv(f, header=False)
-        #time.sleep(.25) # Added cause manners count
-        # Manners would mean this takes a month to run...hmmm
+        time.sleep(.25) # Added cause manners count. Embedded only where we are actually saving info.
+    
